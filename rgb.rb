949c995701e75e5ab4ebbdfee93fc607986ce212
gabolucuy <gabolucuy@hotmail.com>
2017-02-21 16:55:05 -0400

    class rgb created

---
+++ rgb.rb
@@ -0,0 +1,13 @@
+class rgb
+
+  attr_accessor :red
+  attr_accessor :green
+  attr_accessor :blue
+
+	def initialize (red, green, blue)
+		@red = red.to_f
+		@green = green.to_f
+		@blue = blue.to_f
+	end
+
+end
