1501ee0052c2333f5daf4c46e236d569f6814681
gabolucuy <gabolucuy@hotmail.com>
2017-03-15 00:12:41 -0400

    metodo intersection?

--- main.rb
+++ main.rb
@@ -1,21 +1,30 @@
 require_relative 'vector.rb'
 require_relative 'camera.rb'
-vector1 = Vector.new(1,2,3)
-vector2 = Vector.new(45,78,23)
+require_relative 'Sphere.rb'
+require_relative 'Ray.rb'
 
 
-e = Vector.new(1,2,-6)
-center = Vector.new(1,2,-4)
-up = Vector.new(0,1,0)
-fov = 39.0
-df = 1.0
+#Valores de la cámara
+e= Vector.new(278,273,-800)
+center= Vector.new(278,273,-700)
+up= Vector.new(0,1,0)
+fov= 39.31
+df=0.035
+
 nx = 640.0
 ny = 480.0
 
-camera = Camera.new(e,center,up,fov,df)
+# Valores de la esfera
+position = Vector.new(370,120,370)
+radius = 120
+
+camera = Camera.new(e, center, up, fov, df)
+sphere = Sphere.new(position, radius)
+
 (0...nx).each do |i|
-  (0...ny).each do |j|
-    dir = camera.calcularDistancia(i,j,nx,ny)
-    dir.show
-  end
+ (0...ny).each do |j|
+   dir = camera.calcularDistancia(i,j,nx,ny)
+   ray = Ray.new(e, dir)
+   sphere.intersection?(ray)
+ end
 end
--- sphere.rb
+++ sphere.rb
@@ -1,12 +1,21 @@
 class Sphere
 require_relative 'vector.rb'
-	attr_accessor :radius :position
+	attr_accessor :radius, :position
 
-	def initialize (radius,position)
+	def initialize (position,radius)
 		@position = position
-		@radius = radius.tog
+		@radius = radius.to_f
 	end
 
-	
+	def intersection? (ray)
+		e = ray.position
+		d = ray.direction
+		a = d.productoEscalar(d)
+		ec = e.resta_vector(@position)
+		b = d.num_product(2).productoEscalar(ec)
+		c = ec.productoEscalar(ec) - @radius**2
+
+		puts "(A:#{a} B:#{b} C:#{c})"
+	end
 
 end
