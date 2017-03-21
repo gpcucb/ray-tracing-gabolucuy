0eb999df78132aba8d2498653bcc5f0e686bca3f
gabolucuy <gabolucuy@hotmail.com>
2017-03-21 19:17:43 -0400

    pequenia correcion en clase triangle

--- triangle.rb
+++ triangle.rb
@@ -14,12 +14,15 @@ class Triangle
     a = @a.x - @b.x
     b = @a.y - @b.y
     c = @a.z - @b.z
+
     d = @a.x - @c.x
     e = @a.y - @c.y
     f = @a.z - @c.z
+
     g = d_ray.x
     h = d_ray.y
     i = d_ray.z
+
     j = @a.x - e_ray.x
     k = @a.y - e_ray.y
     l = @a.z - e_ray.z
@@ -27,7 +30,7 @@ class Triangle
     m = a*(e*i - h*f) + b*(g*f - d*i) + c*(d*h - e*g)
     beta = (j * (e*i - h*f) + k*(g*f - d*i) + l*(d*h - e*g)) / m
     gama = (i * (a*k - j*b) + h*(j*c - a*l) + g*(b*l - k*c)) / m
-    t = (f * (a*k - j*b) + e*(j*c - a*l) + d*(b*l - k*c)) / m
+    t = -1*((f * (a*k - j*b) + e*(j*c - a*l) + d*(b*l - k*c)) / m)
 
     if ( beta>0.0 && gama>0.0 && beta+gama <1.0 )
       puts "(Beta:#{beta} Gama:#{gama} t:#{t})"
